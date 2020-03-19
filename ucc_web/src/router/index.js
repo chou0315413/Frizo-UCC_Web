e11f9ba4f8d9ab7c0a57e3f936fed78c9e1ed7e9
wu-shang-ru <[best0049@gmail.com]>
2020-03-20 02:18:00 +0800

    'upData'

--- ucc_web/src/router/index.js
+++ ucc_web/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router'
 import HomePage from "../pages/HomePage";
 import Login from "../components/Login";
 import Oauth2Redirect from "../pages/Oauth2Redirect";
+import UserInfo from "../components/UserInfo";
+import page404 from "../pages/page404";
 
 Vue.use(VueRouter)
 
@@ -23,6 +25,16 @@ export default new VueRouter({
             path: '/oauth2/redirect',
             name: 'oauth2',
             component: Oauth2Redirect
+        },
+        {
+            path: '/UserInfo',
+            name: 'UserInfo',
+            component: UserInfo
+        },
+        {
+            path: '/*',
+            name: page404,
+            component: page404
         }
     ]
 })
