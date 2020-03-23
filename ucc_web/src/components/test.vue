391a68a1ed7445fe586712d06c15d15aa0aff5f9
wu-shang-ru <[best0049@gmail.com]>
2020-03-24 02:36:37 +0800

    test

--- ucc_web/src/components/test.vue
+++ ucc_web/src/components/test.vue
@@ -1,80 +1,177 @@
 <template>
-  <div class="register">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg"></div>
-        <div class="col-lg-6 registerPage">
-          <h4>歡迎註冊UCC帳號</h4>
-          <div class="input-group input-group-sm logInDivPading mt-4 inputBorder">
-            <input
-              type="text"
-              id="userEmail"
-              class="form-control textLetterSpacing"
-              placeholder="輸入您的 E-mail 信箱"
-              aria-describedby="inputGroup-sizing-sm"
-              v-model="userEmail"
-            />
-          </div>
-          <div class="input-group input-group-sm logInDivPading mb-3 mt-4 inputBorder">
-            <input
-              type="password"
-              id="userPassword"
-              class="form-control textLetterSpacing"
-              placeholder="輸入您的密碼"
-              aria-describedby="inputGroup-sizing-sm"
-              v-model="userPassword"
-            />
-          </div>
-          <div class="notice">請使用英文字母及阿拉伯數字和部分特殊符號</div>
-          <div class="registerBtn">
-            <button type="button" class="btn btn-link mt-4">註冊</button>
-          </div>
-        </div>
-        <div class="col-lg"></div>
-      </div>
-    </div>
+  <div class="Navbar">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-nav class="nav-menu">
+        <b-nav-item id="bar-links">
+          <a href="#">menu content</a>
+          <a href="#">menu content</a>
+          <a href="#">menu content</a>
+        </b-nav-item>
+        <b-nav-item class="icon" onclick="clickfunction()">
+          <font-awesome-icon icon="bars" size="lg" />
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-brand href="#">Student Club for NTUB</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- <b-navbar-nav>
+                <b-nav-item href="#">Link</b-nav-item>
+                <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>-->
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <!-- Search Bar -->
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              id="search_box"
+              value="Search"
+              onfocus="if(this.value='Search') this.value=''"
+              onblur="if(this.value='') this.value='Search'"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0 mr-5" type="submit">
+              <font-awesome-icon icon="search" size="lg" />
+            </b-button>
+          </b-nav-form>
+
+          <!-- Chat Button -->
+          <b-nav-item class="nav-item mr-2" href="#">
+            <font-awesome-icon icon="comment" size="lg" />
+            <span class="nav-font">Chat</span>
+          </b-nav-item>
+          <!-- friend-list DropDown -->
+          <b-nav-item-dropdown right>
+            <template v-slot:button-content>
+              <font-awesome-icon icon="user-friends" size="lg" />
+              <span class="nav-font">Friends</span>
+            </template>
+            <b-dropdown-item href="#">##</b-dropdown-item>
+            <b-dropdown-item href="#">##</b-dropdown-item>
+            <b-dropdown-item href="#">##</b-dropdown-item>
+            <b-dropdown-item href="#">##</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- User DropDown -->
+            <template v-slot:button-content>
+              <font-awesome-icon icon="user-circle" size="lg" />
+              <span class="nav-font">User</span>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign in</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <Login></Login>
+    <div class="test"></div>
   </div>
 </template>
 
 <script>
+import Login from "@/components/Login";
+
+import { signin } from "@/api/auth";
+import { mapActions, mapGetters } from "vuex";
+import jquery from "jquery";
+
 export default {
-  name: "register",
+  name: "Navbar",
+
   data() {
     return {
-      userEmail: "",
-      userPassword: ""
+      email: "",
+      password: "",
+      token: {
+        tokenType: "",
+        accessToken: ""
+      }
     };
+  },
+
+  components: {
+    Login
+  },
+
+  methods: {
+    closeModal() {
+      jquery("#Login").modal("toggle");
+    },
+
+    login() {
+      var userInfo = {
+        email: this.email,
+        password: this.password
+      };
+      var jsonData = userInfo;
+      signin(jsonData)
+        .then(resp => {
+          this.token.tokenType = resp.data.tokenType;
+          this.token.accessToken = resp.data.accessToken;
+          const token = this.token;
+          this.storeToken(token);
+          this.closeModal();
+          this.$router.push("/");
+        })
+        .catch(err => {
+          console.log(err.message);
+          alert("抱歉，您輸入帳密有誤喔!");
+        });
+    },
+
+    ...mapActions({
+      storeToken: "auth/login"
+    }),
+
+    clickfunction: function() {
+      var x = document.getElementById("bar-links");
+      if (x.style.display == "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+  },
+  computed: {
+    ...mapGetters({
+      userInfo: "auth/userInfo"
+    })
   }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h4 {
-  margin-bottom: 50px;
+.Navbar {
+  position: fixed;
+  width: 100%;
+  top: 0px;
 }
-.registerPage {
+#bar-links {
+  display: none;
+}
+.nav-menu {
+  margin-right: 5px;
+}
+.nav-menu a {
+  display: block;
+}
+.nav-menu a.icon {
+  display: block;
+  color: white;
+}
+
+.modal-backdrop {
+  z-index: -1;
+}
+.test {
   position: relative;
-  height: 460px;
-  box-shadow: 0 0 10px #000000;
-  padding-top: 60px;
-  background-image: url(../assets/signInBackground/login.jpg);
-  background-size: cover;
-}
-.logInDivPading {
-  position: relative;
-  margin: auto;
-  width: 350px;
-}
-.inputBorder {
-  border: 1px #747474 solid;
-}
-.notice {
-  position: relative;
-  font-size: 10px;
-  left: 80px;
-}
-.registerBtn {
-  margin-top: 25px;
+  height: 2000px;
+  background-color: aqua;
 }
 </style>
