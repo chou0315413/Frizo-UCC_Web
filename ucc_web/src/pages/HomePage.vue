41da86633911f80673d25e758af62e36a92f8231
wu-shang-ru <[best0049@gmail.com]>
2020-03-29 14:51:11 +0800

    addBanner

--- ucc_web/src/pages/HomePage.vue
+++ ucc_web/src/pages/HomePage.vue
@@ -2,6 +2,7 @@
   <div class="HomePage">
     <Navbar></Navbar>
     <nav-header></nav-header>
+    <Banner></Banner>
     <ContentList></ContentList>
     <router-link to="/store">到 Store 測試頁</router-link>
   </div>
@@ -11,13 +12,15 @@
 import NavHeader from "@/components/NavHeader";
 import ContentList from "@/components/ContentList";
 import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
 
 export default {
   name: "HomePage",
   components: {
     NavHeader,
     ContentList,
-    Navbar
+    Navbar,
+    Banner
   }
 };
 </script>
