8da60e559f2df9581cc76ae4c47fedcd689f6708
wu-shang-ru <[best0049@gmail.com]>
2020-04-06 19:32:01 +0800

    upData

--- ucc_web/src/store/modules/user/mutations.js
+++ ucc_web/src/store/modules/user/mutations.js
@@ -2,7 +2,7 @@ import * as types from './mutation-types'
 
 // Mutation 裡面的方法必須是同步的
 const mutations = {
-    [types.SET_USER_INFO] (state, {userName, userId, imgUrl, email, provider}) {
+    [types.SET_USER_INFO](state, { userName, userId, imgUrl, email, provider }) {
         console.log(`set User Info in mutations`);
         state.userInfo.userName = userName;
         state.userInfo.userId = userId;
