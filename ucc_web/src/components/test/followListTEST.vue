1d1ab8ace2729833773d2451b957d113e759ae9c
wu-shang-ru <[best0049@gmail.com]>
2020-05-26 12:37:25 +0800

    content.vue_updata

---
+++ ucc_web/src/components/test/followListTEST.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="followList">
+    <div class="activity" v-for="index in followList" :key="index">
+      <div v-for="index in followList" :key="index">
+        <div v-if="index.index%2 == 1" class="item">
+          <div class="imgBox">
+            <img :src="index.img" class="itemImg" />
+          </div>
+          <div class="itemContent">
+            <div class="itemTitle">{{index.title}}</div>
+            <div class="itemIntroduction mb-4">
+              <div>{{index.message}}</div>
+            </div>
+            <div class="deadline">活動截止日期：{{index.deadline}}</div>
+            <div class="joinPeople">目前參加人數：{{index.joinPeople}}人</div>
+            <div class="tag"></div>
+          </div>
+        </div>
+      </div>
+      <div v-for="index in followList" :key="index">
+        <div v-if="index.index%2 == 0" class="item">
+          <div class="imgBox">
+            <img :src="index.img" class="itemImg" />
+          </div>
+          <div class="itemContent">
+            <div class="itemTitle">{{index.title}}</div>
+            <div class="itemIntroduction mb-4">
+              <div>{{index.message}}</div>
+            </div>
+            <div class="deadline">活動截止日期：{{index.deadline}}</div>
+            <div class="joinPeople">目前參加人數：{{index.joinPeople}}人</div>
+            <div class="tag"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "followList",
+  data() {
+    return {
+      followList: [
+        {
+          index: 1,
+          title: "聯網未來挑戰賽(follow)",
+          message:
+            "快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!",
+          deadline: "8月8號",
+          joinPeople: "12",
+          tag: "",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/f5eac69d-0e16-46f9-b228-f75a15f77c3c_640x640.png"
+        },
+        {
+          index: 2,
+          title: "「舜間發現愛，興善攝光彩」廟會創意攝影與創",
+          message:
+            "木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人!",
+          deadline: "8月8號",
+          joinPeople: "12",
+          tag: "",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/8ae04293-cd11-40d0-9040-d7578369dcd6_640x640.png"
+        },
+        {
+          index: 3,
+          title: "WHO需要你的關心？關懷譚先生大賽",
+          message:
+            "火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人",
+          deadline: "8月8號",
+          joinPeople: "12",
+          tag: "",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/91e55cfd-2414-4230-a830-9adf72a980a2_640x640.jpg"
+        },
+        {
+          index: 4,
+          title: "藝術智能之「我眼中的抗疫英雄」創作繪畫比賽",
+          message:
+            "隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的",
+          deadline: "8月8號",
+          joinPeople: "12",
+          tag: "",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/7e4ebc1b-1ff2-4e8b-aa8d-7fc42cf045e4_640x640.png"
+        },
+        {
+          index: 5,
+          title: "109年使用牌照稅網路有獎徵答",
+          message: "快來參加!",
+          deadline: "8月8號",
+          joinPeople: "12",
+          tag: "",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/9773bdd1-948f-4d66-9cde-0c1854f3a51f_640x640.png"
+        },
+        {
+          index: 6,
+          title: "熱舞社期末展",
+          message: "不見不散!",
+          deadline: "8月8號",
+          joinPeople: "12",
+          tag: "",
+          img: "/def"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.activity {
+  height: 450px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+}
+
+.item {
+  position: relative;
+  display: grid;
+  grid-column: 1/2;
+  grid-row: 1/2;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  height: 400px;
+  width: 430px;
+  box-shadow: 0 0 3px #2e2e2e;
+  background-color: #ffffff;
+  margin: 20px 0px;
+}
+
+.item2 {
+  position: relative;
+  display: grid;
+  grid-column: 2/3;
+  grid-row: 1/2;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  height: 400px;
+  width: 430px;
+  box-shadow: 0 0 3px #2e2e2e;
+  background-color: #ffffff;
+  margin: 20px 0px;
+}
+
+.imgBox {
+  padding: 5px;
+  grid-column: 1/9;
+  grid-row: 1/4;
+}
+
+.itemImg {
+  height: 100%;
+  width: 100%;
+}
+
+.itemContent {
+  grid-column: 1/9;
+  grid-row: 4/9;
+  padding: 0px 15px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+}
+
+.itemTitle {
+  font-size: 28px;
+  grid-column: 1/5;
+  grid-row: 1/2;
+  text-align: left;
+}
+
+.itemIntroduction {
+  text-align: left;
+  height: 35px;
+  grid-column: 1/5;
+  grid-row: 2/4;
+}
+
+.deadline {
+  grid-column: 1/3;
+  grid-row: 4/5;
+  text-align: left;
+}
+
+.joinPeople {
+  text-align: left;
+  grid-column: 1/3;
+  grid-row: 4/5;
+  position: relative;
+  top: 50%;
+}
+
+.tag {
+  position: relative;
+  top: -5px;
+  grid-column: 3/5;
+  grid-row: 4/6;
+  background-color: blue;
+}
+
+@media (max-width: 650px) {
+  .itemTitle {
+    font-size: 22px;
+  }
+}
+</style>
---
+++ ucc_web/src/components/test/hotListTEST.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="hotList">
+    <div class="item" v-for="index in hotList" :key="index">
+      <div class="imgBox">
+        <img :src="index.img" class="itemImg" />
+      </div>
+      <div class="itemContent">
+        <div class="itemTitle">{{index.title}}</div>
+        <div class="itemIntroduction mb-4">
+          <div>{{index.message}}</div>
+        </div>
+        <div class="itemAttention">
+          關注度：12
+          <br />留言數：23
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hotList",
+  data() {
+    return {
+      hotList: [
+        {
+          title: "聯網未來挑戰賽(hotList)",
+          message:
+            "快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!快來參加!",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/f5eac69d-0e16-46f9-b228-f75a15f77c3c_640x640.png"
+        },
+        {
+          title: "「舜間發現愛，興善攝光彩」廟會創意攝影與創",
+          message:
+            "木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人木頭人!",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/8ae04293-cd11-40d0-9040-d7578369dcd6_640x640.png"
+        },
+        {
+          title: "WHO需要你的關心？關懷譚先生大賽",
+          message:
+            "火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人火星人",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/91e55cfd-2414-4230-a830-9adf72a980a2_640x640.jpg"
+        },
+        {
+          title: "藝術智能之「我眼中的抗疫英雄」創作繪畫比賽",
+          message:
+            "隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的隨機的",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/7e4ebc1b-1ff2-4e8b-aa8d-7fc42cf045e4_640x640.png"
+        },
+        {
+          title: "109年使用牌照稅網路有獎徵答",
+          message: "快來參加!",
+          img:
+            "https://files.bountyhunter.co/contest/public/202004/9773bdd1-948f-4d66-9cde-0c1854f3a51f_640x640.png"
+        },
+        {
+          title: "熱舞社期末展",
+          message: "不見不散!",
+          img: "/def"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.item {
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  height: 170px;
+  width: 100%;
+  box-shadow: 0 0 3px #2e2e2e;
+  background-color: #ffffff;
+  margin: 20px 0px;
+}
+.itemImg {
+  height: 160px;
+  width: 100%;
+}
+.imgBox {
+  padding: 0px;
+  grid-column: 1/2;
+  grid-row: 1/4;
+}
+.itemContent {
+  grid-column: 2/5;
+  grid-row: 1/4;
+  padding: 0px 15px;
+}
+.itemTitle {
+  text-align: left;
+  font-size: 28px;
+  grid-column: 2/5;
+  grid-row: 1/2;
+}
+.itemIntroduction {
+  text-align: left;
+  height: 35px;
+}
+.itemAttention {
+  text-align: right;
+}
+
+@media (max-width: 650px) {
+  .itemTitle {
+    font-size: 22px;
+  }
+}
+</style>
