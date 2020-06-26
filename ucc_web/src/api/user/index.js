50d4920d14a77af30e5e891cc45d31d6c68a977f
chou0315413 <10646023@ntub.edu.tw>
2020-06-26 20:07:01 +0800

    api of getOtherUserInfo and findUser

--- ucc_web/src/api/user/index.js
+++ ucc_web/src/api/user/index.js
@@ -1,66 +1,111 @@
-import { getRequest, postRequest, uploadFileRequest } from '../../utils/UccSender'
+import {
+  getRequest,
+  postRequest,
+  uploadFileRequest,
+} from "../../utils/UccSender";
 
-export const getUserInfo = () => getRequest('/user/me');
+export const getUserInfo = () => getRequest("/user/me");
 
-export const sendVerifyCodeEmail = () => getRequest('/user/send/email/verify');
+export const sendVerifyCodeEmail = () => getRequest("/user/send/email/verify");
 
-export const checkVerifyCodeEmail = code => {
-    let params = {
-        verifyCode: code
-    };
-    return getRequest('/user/check/email/verify', params)
+export const checkVerifyCodeEmail = (code) => {
+  let params = {
+    verifyCode: code,
+  };
+  return getRequest("/user/check/email/verify", params);
 };
 
-
 // 更新 User Profile 的 API。(不包括 avatar 與 background)
-export const updateUserInfo = ({ address, collageLocation, collageName, gender, grade, majorSubject, name, phoneNumber }) => {
-    let actionUrl = "/user/update/userinfo";
-    let params = {};
-    if (name != null) {
-        params.name = name;
-    }
-    if (address != null) {
-        params.address = address;
-    }
-    if (collageLocation != null) {
-        params.collageLocation = collageLocation;
-    }
-    if (collageName != null) {
-        params.collageName = collageName;
-    }
-    if (gender != null) {
-        params.gender = gender;
-    }
-    if (grade != null) {
-        params.grade = grade;
-    }
-    if (majorSubject != null) {
-        params.majorSubject = majorSubject;
-    }
-    if (phoneNumber != null) {
-        params.phoneNumber = phoneNumber;
-    }
-    return postRequest(actionUrl, params)
+export const updateUserInfo = ({
+  address,
+  collageLocation,
+  collageName,
+  gender,
+  grade,
+  majorSubject,
+  name,
+  phoneNumber,
+}) => {
+  let actionUrl = "/user/update/userinfo";
+  let params = {};
+  if (name != null) {
+    params.name = name;
+  }
+  if (address != null) {
+    params.address = address;
+  }
+  if (collageLocation != null) {
+    params.collageLocation = collageLocation;
+  }
+  if (collageName != null) {
+    params.collageName = collageName;
+  }
+  if (gender != null) {
+    params.gender = gender;
+  }
+  if (grade != null) {
+    params.grade = grade;
+  }
+  if (majorSubject != null) {
+    params.majorSubject = majorSubject;
+  }
+  if (phoneNumber != null) {
+    params.phoneNumber = phoneNumber;
+  }
+  return postRequest(actionUrl, params);
 };
 
 // 上傳照片 API
 export const updateUserAvatar = (avatar) => {
-    if (avatar == null) {
-        return;
-    }
-    let actionUrl = "/user/update/avatar";
-    let formData = new FormData();
-    formData.append('avatar', avatar);
-    return uploadFileRequest(actionUrl, formData)
+  if (avatar == null) {
+    return;
+  }
+  let actionUrl = "/user/update/avatar";
+  let formData = new FormData();
+  formData.append("avatar", avatar);
+  return uploadFileRequest(actionUrl, formData);
 };
 
 // 上傳 bg API
 export const updateProfileBackground = (background) => {
-    if (background == null) {
-        return;
-    }
-    let actionUrl = "/user/update/background";
-    let formData = new FormData();
-    formData.append('background', background);
-    return uploadFileRequest(actionUrl, formData)
+  if (background == null) {
+    return;
+  }
+  let actionUrl = "/user/update/background";
+  let formData = new FormData();
+  formData.append("background", background);
+  return uploadFileRequest(actionUrl, formData);
+};
+
+// 尋找使用者 API
+export const findUser = (keywords, page) => {
+  let actionUrl = "/user/find";
+  let params = {};
+
+  if (keywords == null || keywords == "") {
+    return;
+  } else {
+    params.keywords = keywords;
+  }
+
+  if (page == null || page == "") {
+    params.page = 10;
+  } else {
+    params.page = page;
+  }
+
+  return getRequest(actionUrl, params);
+};
+
+//取得使用者資料 API
+export const getOtherUserInfo = (id) => {
+  let actionUrl = "user/get/info/" + id;
+  let params = {};
+  if (id == null) {
+    return;
+  } else {
+    params.id = id;
+  }
+
+  return getRequest(actionUrl, params);
 };
